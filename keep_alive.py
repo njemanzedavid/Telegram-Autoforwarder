643badf71d5c4a1ca50fdf64a43d84a9d71975af
njemanzedavid <njemanzedavid722@gmail.com>
2024-10-04 10:32:59 +0100

    Update keep_alive.py
    commented out

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,14 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+#from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class KeepAliveHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b"I'm alive")
+#class KeepAliveHandler(BaseHTTPRequestHandler):
+    #def do_GET(self):
+        #self.send_response(200)
+        #self.send_header('Content-type', 'text/plain')
+        #self.end_headers()
+        #self.wfile.write(b"I'm alive")
 
-def start_keep_alive():
-    server_address = ('', 8080)
-    httpd = HTTPServer(server_address, KeepAliveHandler)
-    print('Keep alive server running on port 8080...')
-    httpd.serve_forever()
+#def start_keep_alive():
+    #server_address = ('', 8080)
+    #httpd = HTTPServer(server_address, KeepAliveHandler)
+    #print('Keep alive server running on port 8080...')
+    #httpd.serve_forever()
